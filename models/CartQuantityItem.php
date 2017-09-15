c24ba518d6ee87fa88ce6a22dcfb0051340f9276
FranziskaKoehn <franziska.koehn@haendlerbund.de>
2017-09-15 10:38:42 +0200

    introduce ability to provide product amount

---
+++ models/CartQuantityItem.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: franziskakoehn
+ * Date: 15.09.17
+ * Time: 14:33
+ */
+
+namespace yii2mod\cart\models;
+
+
+trait CartQuantityItem
+{
+    public $quantity = 0;
+
+    public function setQuantity($qty){
+        $this->quantity = $qty;
+    }
+
+}
